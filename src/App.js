ab01ba5ba490b8e4162da3e2a2994d54a3dd2f9c
kiranmadhe <kiran@localhost.localdomain>
2022-04-05 17:48:13 +0530

    router is used in app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import { render } from '@testing-library/react';
 import Navbar from './components/Navbar';
 import Newsitem from './components/Newsitem';
 import News from './components/News';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 
 export default class App extends Component {
@@ -12,18 +17,45 @@ export default class App extends Component {
 
 
 
+  // render() {
+
+
+  //   return (
+
+  //     <>
+
+  //      <Navbar/>
+  //      <News pageSize={9} country="in" category='general'/>
+  //     </>
+  //   )
+
+  // }
+
   render() {
-
-    console.log("render function")
     return (
-
       <>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route exact path="/general" element={<News key="general" pageSize={9} country="in" category='general' />}></Route>
+            <Route exact path="/business" element={<News key="business" pageSize={9} country="in" category='business' />}></Route>
+            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={9} country="in" category='entertainment' />}></Route>
+            <Route exact path="/health" element={<News key="health" pageSize={9} country="in" category='health' />}></Route>
+            <Route exact path="/science" element={<News key="science" pageSize={9} country="in" category='science' />}></Route>
+            <Route exact path="/sports" element={<News key="sports" pageSize={9} country="in" category='sports' />}></Route>
+            <Route exact path="/technology" element={<News key="technology" pageSize={9} country="in" category='technology' />}></Route>
 
-       <Navbar/>
-       <News pageSize={9} country="in" category='general'/>
+
+
+          </Routes>
+
+
+
+
+        </BrowserRouter>
+
       </>
     )
-
   }
 }
 
